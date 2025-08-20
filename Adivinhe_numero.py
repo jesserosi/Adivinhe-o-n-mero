86060ada9943a4cd4f02c9701f4dde06a57cb9ca
Jéssica Priscila <jesik1611@gmail.com>
2025-08-20 17:59:21 -0300

    Primeira aula

---
+++ Adivinhe_numero.py
@@ -0,0 +1,23 @@
+
+import random
+
+numero_aleatorio = random.randint(1, 10)
+tentativas = 0
+acertou = False
+
+
+while not acertou:
+        try: 
+            numero = int(input("Digite um numero de 1 a 10:"))
+            tentativas += 1
+
+            if numero  == numero_aleatorio:
+                acertou = True
+                print(f"Parabéns! Você acertou em {tentativas} tentativas.")
+                break
+            elif numero < numero_aleatorio:
+                 print("muito baixo. Tente novamente")
+            elif numero > numero_aleatorio:
+                 print("Muito alto. Tente novamete")
+        except ValueError:
+            print("Entrada inválida. Digite um número inteiro.")
